e039403702600c5bb1aff4f7faf2ff2108f81d3f
Jukra <jukrachai3146@gmail.com>
2024-03-11 20:23:55 +0700

    u0pdate coverage

--- project/staircase/stair_hash.py
+++ project/staircase/stair_hash.py
@@ -1,4 +1,4 @@
-def hash_pyramid(n, k=0):
+def hash_pyramids(n, k=0):
     for i in range(1, n+1):
         for space in range(1, (n-i)+1):
             print(end="")
@@ -10,5 +10,5 @@ def hash_pyramid(n, k=0):
 
 
 n = int(input("Enter number of n: "))
-hash_pyramid(n)
+hash_pyramids(n)
 
--- project/tests/test_hash_pyramid.py
+++ project/tests/test_hash_pyramid.py
@@ -1,19 +1,30 @@
-from staircase.stair_hash import hash_pyramid
 import unittest
+from staircase.stair_hash import hash_pyramids
 
-class PyramidHash(unittest.TestCase):
+class TestHashPyramid(unittest.TestCase):
+    def test_give_2_with_hash_should_be_hh(self):
+        # arrange
+        n = 2
+        pattern = '#'
+        expected_output = "#\n##"
 
-    def test_give_4_with_hash_should_be_hh(self):
-        #arrange
-        n = 4
-        expected_output =  \
-        "#\n" + \
-        "##\n" + \
-        "###\n" + \
-        "####"
+        # act
+        result = hash_pyramids(n, pattern)
 
-        #act
-        result = hash_pyramid(n)
+        # assert
+        self.assertEqual(result, expected_output, f'Should be {expected_output}')
 
-        #assert
-        self.assertEqual(result, f"\n{expected_output}")
+    def test_give_0_with_hash_should_be_hh(self):
+        # arrange
+        n = 0
+        pattern = '#'
+        expected_output = ""
+
+        # act
+        result = hash_pyramids(n, pattern)
+
+        # assert
+        self.assertEqual(result, expected_output, f'Should be {expected_output}')
+
+if __name__ == "__main__":
+    unittest.main()
